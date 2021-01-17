bea66a98d766676ac0f706191f797902334c75b1
up1 <somkiat.p@gmail.com>
2021-01-17 14:53:57 +0700

    Add timezone data

--- workshop-gin-mongodb/cmd/main.go
+++ workshop-gin-mongodb/cmd/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"demo"
-
 	_ "demo/docs"
+	"fmt"
+	"time"
+	_ "time/tzdata"
 )
 
 func main() {
+	fmt.Print("Local time zone ")
+	fmt.Println(time.Now().Zone())
+	fmt.Println(time.Now())
 	demo.StartServer()
 }
